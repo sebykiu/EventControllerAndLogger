d69aea3038093054cb846a397a8cc12076303bb5
Seby Kiunke <seby.kiunke@googlemail.com>
2023-10-15 21:52:25 +0200

    added command line parameter support

--- InfluxPlay/run.py
+++ InfluxPlay/run.py
@@ -4,6 +4,7 @@ import json
 import influxdb_client
 from json_object import Message, Coordinates
 import datetime
+import argparse
 
 
 # Custom encoder to support datetime and Message object
@@ -16,6 +17,7 @@ class CustomEncoder(json.JSONEncoder):
         elif isinstance(obj, datetime.datetime):
             return obj.isoformat()
         return super().default(obj)
+
 
 def retrieve_messages_from_json(filename):
     with open(filename, 'r') as f:
@@ -37,11 +39,10 @@ def retrieve_messages_from_json(filename):
 
             # Convert the timestamp from string to datetime
             timestamp = datetime.datetime.fromtimestamp(float(timestamp))
-            message = Message(sourceId,targetId, objectType, coordinates, timestamp)
+            message = Message(sourceId, targetId, objectType, coordinates, timestamp)
             messages.append(message)
 
     return messages
-
 
 
 def retrieve_messages_from_influx(scenario, org, bucket):
@@ -58,7 +59,7 @@ def retrieve_messages_from_influx(scenario, org, bucket):
 
     # Execute the query
     tables = query_api.query(org=org, query=query)
-    
+
     # Print number of tables returned
     print(f"Number of tables: {len(tables)}")
 
@@ -78,16 +79,12 @@ def retrieve_messages_from_influx(scenario, org, bucket):
             # If all necessary fields are present, create a message
             if sourceId is not None and objectType is not None and x is not None and y is not None and z is not None and timestamp is not None:
                 coordinates = Coordinates(x, y, z)
-                message = Message(sourceId,targetId, objectType, coordinates,  timestamp,scenario,)
+                message = Message(sourceId, targetId, objectType, coordinates, timestamp, scenario, )
                 messages.append(message)
     return messages
 
 
-
-
-
-
-def send_message(ip, port, scenario, org, bucket, use_json = False, json_file=None):
+def send_message(ip, port, scenario, org, bucket, use_json=False, json_file=None):
     # Create a socket object
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -106,28 +103,19 @@ def send_message(ip, port, scenario, org, bucket, use_json = False, json_file=No
 
     print("Loaded {}".format(len(messages)))
 
-  
-
     # If there are no messages, close the socket and return
     if not messages:
         client_socket.close()
         return
 
-    # Get the time of the first message
-    first_message_time = messages[0].Timestamp  # Use the Timestamp attribute
-
     # Calculates the time difference between the current and previous message and let's the execution sleep accordingly
     for i, message in enumerate(messages):
         # First message doesn't have previous message
-        if i == 0:  
+        if i == 0:
             time_diff = 0
         else:
 
-            #print(message.Timestamp)
-            #print(message.Timestamp[i-1].Timestamp)
-            #print(message.Timestamp).total_seconds()
-            #print(message.Timestamp[i-1].Timestamp).total_seconds()
-            time_diff = (message.Timestamp - messages[i-1].Timestamp).total_seconds()
+            time_diff = (message.Timestamp - messages[i - 1].Timestamp).total_seconds()
 
         # Convert the message to JSON using the custom encoder
         message_json = json.dumps(message, cls=CustomEncoder)
@@ -141,7 +129,7 @@ def send_message(ip, port, scenario, org, bucket, use_json = False, json_file=No
         # Send the JSON message to the server
         print('Sending message to Unity')
         client_socket.sendall(encoded_message)
-        print('Going to wait' , time_diff, " seconds")
+        print('Going to wait', time_diff, " seconds")
         # Wait for the calculated time difference before sending the next message
         time.sleep(time_diff)
 
@@ -149,15 +137,21 @@ def send_message(ip, port, scenario, org, bucket, use_json = False, json_file=No
     client_socket.close()
 
 
+if __name__ == "__main__":
+    # Create an argument parser
+    parser = argparse.ArgumentParser(description="Send a message with specific parameters")
 
-# Set the IP address, port, scenario name, org, and bucket
-ip_address = "localhost"  
-port = 54321  
-scenario_name = "vruMec"  
-org = 'rovernet'
-bucket = 'crownet'
-use_json = True
-json_path = 'Scenarios/Freiheit.json'
+    # Define command-line arguments
+    parser.add_argument("--ip", type=str, default="localhost", help="IP address")
+    parser.add_argument("--port", type=int, default=54321, help="Port")
+    parser.add_argument("--scenario", type=str, default="default", help="Scenario name")
+    parser.add_argument("--org", type=str, default="rovernet", help="Organization")
+    parser.add_argument("--bucket", type=str, default="crownet", help="Bucket")
+    parser.add_argument("--use-json", action="store_true", help="Use JSON format")
+    parser.add_argument("--json-path", type=str, default="Scenarios/Freiheit.json", help="Path to JSON file")
 
-# Call the send_message function with the specified parameters
-send_message(ip_address, port, scenario_name, org, bucket,use_json, json_path)
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Call the send_message function with the specified arguments
+    send_message(args.ip, args.port, args.scenario, args.org, args.bucket, args.use_json, args.json_path)
