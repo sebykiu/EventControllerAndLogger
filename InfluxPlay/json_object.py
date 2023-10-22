98dae4a1bcc03dab93a08ab96b3efe00991185d2
Seby Kiunke <seby.kiunke@googlemail.com>
2023-10-22 20:55:22 +0200

    renamed variables to match convention

--- InfluxPlay/json_object.py
+++ InfluxPlay/json_object.py
@@ -1,11 +1,12 @@
 class Message:
-    def __init__(self, id,targetId, objectType, coordinates, timestamp, scenario= None):
+    def __init__(self, id, target_id, object_type, coordinates, timestamp, scenario=None):
         self.SourceId = id
-        self.TargetId = targetId
-        self.ObjectType = objectType
+        self.TargetId = target_id
+        self.ObjectType = object_type
         self.Coordinates = coordinates
         self.Scenario = scenario
         self.Timestamp = timestamp
+
 
 class Coordinates:
     def __init__(self, x, y, z):
