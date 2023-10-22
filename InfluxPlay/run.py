cd055a42932f294726f4d08aaaa81cb928a8c35d
Seby Kiunke <seby.kiunke@googlemail.com>
2023-10-22 21:02:50 +0200

    refactored JSON and InfluxDB message retrieval

--- InfluxPlay/run.py
+++ InfluxPlay/run.py
@@ -6,86 +6,101 @@ from json_object import Message, Coordinates
 import datetime
 import argparse
 import subprocess
+import functools
 
 
 # Custom encoder to support datetime and Message object
+@functools.singledispatch
+def default(obj):
+    if isinstance(obj, Message):
+        return obj.__dict__
+    elif isinstance(obj, Coordinates):
+        return obj.__dict__
+    elif isinstance(obj, datetime.datetime):
+        return obj.isoformat()
+    return super().default(obj)
+
+
 class CustomEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Message):
-            return obj.__dict__
-        elif isinstance(obj, Coordinates):
-            return obj.__dict__
-        elif isinstance(obj, datetime.datetime):
-            return obj.isoformat()
-        return super().default(obj)
+    @staticmethod
+    def default(obj):
+        return default(obj)
 
 
-def retrieve_messages_from_json(filename):
-    with open(filename, 'r') as f:
-        data = json.load(f)
+def retrieve_messages(data_source, scenario=None, org=None, bucket=None, json_path=None):
+    if data_source == "influx":
+        # Create an InfluxDB client
+        client = influxdb_client.InfluxDBClient(url='http://localhost:8086', token='secret-token', org=org)
+        query_api = client.query_api()
 
-    messages = []
-    for record in data:
-        source_id = record.get('SourceId', None)
-        target_id = record.get("TargetId")
-        object_type = record.get('ObjectType', None)
-        coordinates = record.get('Coordinates', None)
-        timestamp = record.get('Timestamp', None)
+        # InfluxDB query to load values for specific scenario and pivot them together for them to be returned in one column
+        query = f'from(bucket: "{bucket}") \
+            |> range(start: 1688234705) \
+            |> filter(fn: (r) => r._measurement == "omnet++" and r.scenario == "{scenario}") \
+            |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value") \
+            |> sort(columns: ["_time"])'
 
-        if source_id is not None and object_type is not None and coordinates is not None and timestamp is not None:
-            x = coordinates.get('X', None)
-            y = coordinates.get('Y', None)
-            z = coordinates.get('Z', None)
-            coordinates = Coordinates(x, y, z)
+        # Execute the query
+        tables = query_api.query(org=org, query=query)
 
-            # Convert the timestamp from string to datetime
-            timestamp = datetime.datetime.fromtimestamp(float(timestamp))
-            message = Message(source_id, target_id, object_type, coordinates, timestamp)
-            messages.append(message)
+        # Print number of tables returned
+        print(f"Number of tables: {len(tables)}")
 
-    return messages
+        # Parse the query result and convert it to a list of messages
+        messages = []
+        for table in tables:
+            print(f"Number of records in this table: {len(table.records)}")
+            for record in table.records:
+                source_id = record.values.get('SourceId', None)
+                target_id = record.values.get('TargetId', None)
+                object_type = record.values.get('ObjectType', None)
+                x = record.values.get('X', None)
+                y = record.values.get('Y', None)
+                z = record.values.get('Z', None)
+                timestamp = record.values.get('_time', None)
 
+                # If all necessary fields are present, create a message
+                if source_id is not None and object_type is not None and x is not None and y is not None and z is not None and timestamp is not None:
+                    coordinates = Coordinates(x, y, z)
+                    message = Message(source_id, target_id, object_type, coordinates, timestamp, scenario)
+                    messages.append(message)
+    elif data_source == "json":
+        with open(json_path, 'r') as f:
+            data = json.load(f)
 
-def retrieve_messages_from_influx(scenario, org, bucket):
-    # Create an InfluxDB client
-    client = influxdb_client.InfluxDBClient(url='http://localhost:8086', token='secret-token', org=org)
-    query_api = client.query_api()
+        messages = []
+        for record in data:
+            source_id = record.get('SourceId', None)
+            target_id = record.get("TargetId")
+            object_type = record.get('ObjectType', None)
+            coordinates = record.get('Coordinates', None)
+            timestamp = record.get('Timestamp', None)
 
-    # InfluxDB query to load values for specific scenario and pivot them together for them to be returned in one column
-    query = f'from(bucket: "{bucket}") \
-        |> range(start: 1688234705) \
-        |> filter(fn: (r) => r._measurement == "omnet++" and r.scenario == "{scenario}") \
-        |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value") \
-        |> sort(columns: ["_time"])'
-
-    # Execute the query
-    tables = query_api.query(org=org, query=query)
-
-    # Print number of tables returned
-    print(f"Number of tables: {len(tables)}")
-
-    # Parse the query result and convert it to a list of messages
-    messages = []
-    for table in tables:
-        print(f"Number of records in this table: {len(table.records)}")
-        for record in table.records:
-            source_id = record.values.get('SourceId', None)
-            target_id = record.values.get('TargetId', None)
-            object_type = record.values.get('ObjectType', None)
-            x = record.values.get('X', None)
-            y = record.values.get('Y', None)
-            z = record.values.get('Z', None)
-            timestamp = record.values.get('_time', None)
-
-            # If all necessary fields are present, create a message
-            if source_id is not None and object_type is not None and x is not None and y is not None and z is not None and timestamp is not None:
+            if source_id is not None and object_type is not None and coordinates is not None and timestamp is not None:
+                x = coordinates.get('X', None)
+                y = coordinates.get('Y', None)
+                z = coordinates.get('Z', None)
                 coordinates = Coordinates(x, y, z)
-                message = Message(source_id, target_id, object_type, coordinates, timestamp, scenario)
+
+                # Convert the timestamp from string to datetime
+                timestamp = datetime.datetime.fromtimestamp(float(timestamp))
+                message = Message(source_id, target_id, object_type, coordinates, timestamp)
                 messages.append(message)
+
     return messages
 
 
-def send_message(ip, port, scenario, org, bucket, json_path=None):
+def send_message_length(client_socket, message_length):
+    # Send the length of the message to the server
+    client_socket.sendall(message_length)
+
+
+def send_message_json(client_socket, message_json):
+    # Send the JSON message to the server
+    client_socket.sendall(message_json.encode())
+
+
+def send_message(ip, port, scenario, org, bucket, json_path=None, debug=False):
     # Create a socket object
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -117,17 +132,20 @@ def send_message(ip, port, scenario, org, bucket, json_path=None):
 
         # Convert the message to JSON using the custom encoder
         message_json = json.dumps(message, cls=CustomEncoder)
-        encoded_message = message_json.encode()
 
         # Calculate the length of the encoded message and convert it to bytes
-        message_length = len(encoded_message).to_bytes(4, byteorder='big')
+        message_length = len(message_json.encode()).to_bytes(4, byteorder='big')
 
         # Send the length of the message to the server
-        client_socket.sendall(message_length)
+        send_message_length(client_socket, message_length)
+
         # Send the JSON message to the server
-        print('Sending message to Unity')
-        client_socket.sendall(encoded_message)
-        print('Going to wait', time_diff, " seconds")
+        send_message_json(client_socket, message_json)
+
+        if debug:
+            print('Sending message to Unity')
+            print('Time difference:', time_diff)
+
         # Wait for the calculated time difference before sending the next message
         time.sleep(time_diff)
 
@@ -164,4 +182,4 @@ if not (args.scenario is None) ^ (args.json_path is None):
     parser.error("Please provide either --scenario or --json-path, but not both.")
 
 # Call the send_message function with the specified arguments
-send_message(args.ip, args.port, args.scenario, args.org, args.bucket, args.json_path)
+send_message(args.ip, args.port, args.scenario, args.org, args.bucket, args.json_path, args.debug)
