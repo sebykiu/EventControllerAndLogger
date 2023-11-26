870585826db0a2d0f7c586ae879d63fb98c771bd
skiunke <seby.kiunke@googlemail.com>
2023-11-26 11:21:22 +0100

    added fix to edge case for unrecognized network

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+NETWORK_NAME="rovernet"
+
+if docker network inspect "$NETWORK_NAME" >/dev/null 2>&1; then
+  echo "Network '$NETWORK_NAME' already exists."
+else
+  docker network create "$NETWORK_NAME"
+  echo "Network '$NETWORK_NAME' created."
+fi
+
 docker compose down
 
 docker build -t ecal_image .
