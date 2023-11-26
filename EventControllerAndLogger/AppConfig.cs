b711eff0ce3eddfc02bbf78d8776bf380d0e40ca
skiunke <seby.kiunke@googlemail.com>
2023-11-26 11:50:51 +0100

    removed unity forwarding

--- EventControllerAndLogger/AppConfig.cs
+++ EventControllerAndLogger/AppConfig.cs
@@ -2,11 +2,7 @@ namespace EventControllerAndLogger;
 
 public class AppConfig
 {
-    public bool UseCrownet { get; set; }
-    public bool UseInflux { get; set; }
     public int OmnetPort { get; set; }
-    public string UnityAddr { get; set; }
-    public int UnityPort { get; set; }
     public string InfluxAddr { get; set; }
     public int InfluxPort { get; set; }
 
